9d1012a54d8a640e048890857a0250bf90f28152
Mark Souza <marksanto.dev@gmail.com>
2022-02-19 16:08:31 -0300

    Project initiation

---
+++ src/main/java/com/marksouzza/systemproject/SystemprojectApplication.java
@@ -0,0 +1,13 @@
+package com.marksouzza.systemproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marksouzza/systemproject/SystemprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marksouzza.systemproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
