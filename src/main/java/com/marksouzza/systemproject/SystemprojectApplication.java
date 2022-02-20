e38740b7f072657b1c36fcf78765b85c18f0eac5
Mark Souza <marksanto.dev@gmail.com>
2022-02-20 15:23:50 -0300

    Create Instance Operation

--- src/main/java/com/marksouzza/systemproject/SystemprojectApplication.java
+++ src/main/java/com/marksouzza/systemproject/SystemprojectApplication.java
@@ -1,12 +1,30 @@
 package com.marksouzza.systemproject;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SystemprojectApplication {
+import com.marksouzza.systemproject.domain.Category;
+import com.marksouzza.systemproject.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class SystemprojectApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SystemprojectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
